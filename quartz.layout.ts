5c427f5a5671d687baa852df235e15bcd7ab5cfb
mint <rezajaxg@gmail.com>
2024-12-08 07:40:03 +0400

    123

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -48,3 +48,18 @@ export const defaultListPageLayout: PageLayout = {
   ],
   right: [],
 }
+
+
+Component.Explorer({
+  mapFn: (node) => {
+    // dont change name of root node
+    if (node.depth > 0) {
+      // set emoji for file/folder
+      if (node.file) {
+        node.displayName = "📄 " + node.displayName
+      } else {
+        node.displayName = "📁 " + node.displayName
+      }
+    }
+  },
+})
